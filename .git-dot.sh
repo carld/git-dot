c7357e69e4536e7809be204f9c0857c2cead7608
Carl Douglas <carl.douglas@gmail.com>
2011-08-21 19:53:08 +1000

    decorate the label, multiline awk script

--- .git-dot.sh
+++ .git-dot.sh
@@ -3,15 +3,22 @@
 function git-dot () {
   BRANCH=`git branch | grep '*' | sed -e 's/\* \(.*\)/\1/'`
 
-  git log --pretty=format:'%h|%p|%s' $@ | 
-    awk -F'|' ' \
-    BEGIN { print "digraph git {"; print "rankdir=BT;";} \
-    { \
+  git log --pretty=format:'%h|%p|%s|%d' $@ | 
+    awk -F'|' '
+    BEGIN { 
+      print "digraph git {"; 
+      print "rankdir=BT;";
+    }
+    {
       gsub(/"/, "", $3)
-      print "node"$1 " [label=\"" $3 "\"];"; \
-      split($2,a," "); \
-      for (i in a) print "node" a[i] " -> " " node" $1 ";"; } \
-    END   { print "}"; } ' > "$BRANCH-graph.dot"
+      print "node"$1 " [label=\"" $3 " " $4 " " "\"];";
+      split($2,a," ");
+      for (i in a) print "node" a[i] " -> " " node" $1 ";"; 
+    }
+    END {
+      print "}";
+    }
+    ' > "$BRANCH-graph.dot"
 
   dot -Tpdf -O "$BRANCH-graph.dot"
 }
